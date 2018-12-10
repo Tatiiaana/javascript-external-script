89b37e5d5af7f124937c8b331dd1679d6f797306
Tatiana Choisy <tatianachoisy@stackerine.com>
2018-12-10 15:36:13 +0100

    js

--- README.md
+++
@@ -1,1 +0,0 @@
-# javascript-external-script
---
+++ scripts/userQueries.js
@@ -0,0 +1,5 @@
+const username = prompt('Quel est votre nom ?');
+const userlastname = prompt('Quel est votre prénom ?');
+const userage = prompt('Quel est votre âge ?');
+
+console.log('Bonjour' + username + userlastname + userage);
